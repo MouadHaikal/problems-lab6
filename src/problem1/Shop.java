94d110a129be44222889655e579ac474a818ccad
Mouad Haikal <Mouad.Haikal@um6p.ma>
2025-11-02 19:43:01 +0100

    Problem 1

--- src/problem1/Shop.java
+++ src/problem1/Shop.java
@@ -2,30 +2,47 @@ package problem1;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Shop
 {
     public static void main (String[] args)
     {
-        ArrayList<Item> cart = new ArrayList();
+        ArrayList<Item> cart = new ArrayList<Item>();
         Item item;
         String itemName;
         double itemPrice;
         int quantity;
+        double total;
         Scanner scan = new Scanner(System.in);
         String keepShopping = "y";
         do
         {
             System.out.print ("Enter the name of the item: ");
-            itemName = scan.nextLine();
+            itemName = scan.next();
             System.out.print ("Enter the unit price: ");
             itemPrice = scan.nextDouble();
             System.out.print ("Enter the quantity: ");
             quantity = scan.nextInt();
-// *** create a new item and add it to the cart
+            // *** create a new item and add it to the cart
+            item = new Item(itemName, itemPrice, quantity);
+            cart.add(item);
             // *** print the contents of the cart object using println
+            
+            System.out.println("----------------Cart----------------");
+            total = 0.0;
+            for (Item itm : cart) {
+                System.out.println("  - " + itm);
+                total += itm.getPrice() * itm.getQuantity();
+            }
+            System.out.println("Total: $" + total);
+            System.out.println("------------------------------------");
+
+
             System.out.print ("Continue shopping (y/n)? ");
-            keepShopping = scan.nextLine();
+            keepShopping = scan.next();
         }
         while (keepShopping.equals("y"));
+
+        scan.close();
     }
 }
